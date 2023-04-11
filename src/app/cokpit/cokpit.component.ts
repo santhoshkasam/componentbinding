ba96f4fdbaa1097672c077d7183fd76f0bd92d7a
Sharada Kasam <sharadakasam@outlook.com>
2023-04-11 09:37:17 +0530

    localref to an element & viewchild

--- src/app/cokpit/cokpit.component.ts
+++ src/app/cokpit/cokpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cokpit',
@@ -11,21 +11,26 @@ export class CokpitComponent implements OnInit {
   newServerContent = '';
  @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
  @Output() blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+ @ViewChild('serverConentInput') serverContentInput: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
   }
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      //serverName: this.newServerName,
+      serverName: nameInput.value,
+      //serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+       //serverName: this.newServerName,
+       serverName: nameInput.value,
+       //serverContent: this.newServerContent
+       serverContent: this.serverContentInput.nativeElement.value
     })
   }
 }
