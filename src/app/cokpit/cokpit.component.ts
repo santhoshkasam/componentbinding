469ae70c60f04c3f46ccd8be0175f89262710aa6
Sharada Kasam <sharadakasam@outlook.com>
2023-04-11 07:55:45 +0530

    componentbinding

---
+++ src/app/cokpit/cokpit.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cokpit',
+  templateUrl: './cokpit.component.html',
+  styleUrls: ['./cokpit.component.css']
+})
+export class CokpitComponent implements OnInit {
+  serverElements = [];
+  newServerName = '';
+  newServerContent = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+}
